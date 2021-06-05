4bd3a921ceb4abaf767dac86b8eb47174795b6a2
Vlaskz <sam.velasquez@hotmail.com>
2021-06-05 19:47:43 -0300

    part one: bradesco, finished.

--- __main__.py
+++ __main__.py
@@ -1,29 +1,67 @@
 from openpyxl import Workbook, load_workbook
 import man, brad #some people certainly will brag about this.
 
-#open the current working book.
-receber = load_workbook('receber.xlsx', read_only=False)
-
-#clears the old data from the worksheets.
-man.purge(receber['VE09BRAD'],brad.INITIAL_ROW, brad.FINAL_ROW, brad.INITIAL_COLUMN, brad.FINAL_COLUMN)
-man.purge(receber['AV09BRAD'],brad.INITIAL_ROW, brad.FINAL_ROW, brad.INITIAL_COLUMN, brad.FINAL_COLUMN)
-man.purge(receber['VE28BRAD'],brad.INITIAL_ROW, brad.FINAL_ROW, brad.INITIAL_COLUMN, brad.FINAL_COLUMN)
-man.purge(receber['AV28BRAD'],brad.INITIAL_ROW, brad.FINAL_ROW, brad.INITIAL_COLUMN, brad.FINAL_COLUMN)
-receber.save(filename='receber.xlsx')
 
 
 #converts xls worksheets to xlsx - ok
-man.convert_xls_xlsx('c:\\users\\coder\\Desktop\\XL\\VE09.xls')
-man.convert_xls_xlsx('c:\\users\\coder\\Desktop\\XL\\AV09.xls')
-man.convert_xls_xlsx('c:\\users\\coder\\Desktop\\XL\\VE28.xls')
-man.convert_xls_xlsx('c:\\users\\coder\\Desktop\\XL\\AV28.xls')
+man.convert_xls_xlsx('c:\\users\\coder\\Desktop\\XL\\VE09.xls',False)
+man.convert_xls_xlsx('c:\\users\\coder\\Desktop\\XL\\AV09.xls',False)
+man.convert_xls_xlsx('c:\\users\\coder\\Desktop\\XL\\VE28.xls',False)
+man.convert_xls_xlsx('c:\\users\\coder\\Desktop\\XL\\AV28.xls',False)
 
 #loads the new datafiles into memory - ok
-ve09 = load_workbook('VE09.xlsx')
-av09 = load_workbook('AV09.xlsx')
-ve28 = load_workbook('VE28.xlsx')
-av28 = load_workbook('AV28.xlsx')
+VE09 = load_workbook('VE09.xlsx')
+AV09 = load_workbook('AV09.xlsx')
+VE28 = load_workbook('VE28.xlsx')
+AV28 = load_workbook('AV28.xlsx')
+RECB = load_workbook('receber.xlsx', read_only=False)
 
-
+#clears the old data from the worksheets.
+man.purge_data(RECB['VE09BRAD'],
+                brad.INITIAL_ROW,
+                brad.FINAL_ROW,
+                brad.INITIAL_COLUMN,
+                brad.FINAL_COLUMN)
+man.purge_data(RECB['AV09BRAD'],
+                brad.INITIAL_ROW,
+                brad.FINAL_ROW,
+                brad.INITIAL_COLUMN,
+                brad.FINAL_COLUMN)
+man.purge_data(RECB['VE28BRAD'],
+                brad.INITIAL_ROW,
+                brad.FINAL_ROW,
+                brad.INITIAL_COLUMN,
+                brad.FINAL_COLUMN)
+man.purge_data(RECB['AV28BRAD'],
+                brad.INITIAL_ROW,
+                brad.FINAL_ROW,
+                brad.INITIAL_COLUMN,
+                brad.FINAL_COLUMN)
+#copy data from datafiles to main workbook
+man.fetch_data(VE09.active,
+                RECB['VE09BRAD'],
+                brad.INITIAL_ROW,
+                brad.FINAL_ROW,
+                brad.INITIAL_COLUMN,
+                brad.FINAL_COLUMN)
+man.fetch_data(AV09.active,
+                RECB['AV09BRAD'],
+                brad.INITIAL_ROW,
+                brad.FINAL_ROW,
+                brad.INITIAL_COLUMN,
+                brad.FINAL_COLUMN)
+man.fetch_data(VE28.active,
+                RECB['VE28BRAD'],
+                brad.INITIAL_ROW,
+                brad.FINAL_ROW,
+                brad.INITIAL_COLUMN,
+                brad.FINAL_COLUMN)
+man.fetch_data(AV28.active,
+                RECB['AV28BRAD'],
+                brad.INITIAL_ROW,
+                brad.FINAL_ROW,
+                brad.INITIAL_COLUMN,
+                brad.FINAL_COLUMN)
+RECB.save(filename='receber.xlsx')
 
 
--- man.py
+++ man.py
@@ -1,7 +1,7 @@
 #man stands for manipulation
 
 #converts xls to xlsx using win32com library and excludes the xls files after successful conversion
-def convert_xls_xlsx(xls):
+def convert_xls_xlsx(xls, flag):
     import win32com.client as win32 
     import os  
     file = xls
@@ -10,15 +10,25 @@ def convert_xls_xlsx(xls):
     wb.SaveAs(file+'x', FileFormat=51) #51 for xlsx/56 for xlsx
     wb.Close()
     excel.Application.Quit()
-    os.system('cmd /c del '+file)
+    if(flag):
+        os.system('cmd /c del '+file)
+        print('Source file has not been deleted.')
     print(file + ' has been converted to '+file+'x!')
 
 
 #fills cells with no data.
 #wb is workbook, sh is sheet, r is row, and c is column
 
-def purge(sh, r_ini, r_fin, c_ini, c_fin):
+def purge_data(sh, r_ini, r_fin, c_ini, c_fin):
     for r in range (r_ini,r_fin):
         for c in range (c_ini, c_fin):
             sh.cell(column=c, row=r).value = ""
-    print(sh , ' has been purged!')   
+    print(sh , ' has been purged!') 
+
+
+def fetch_data(source, destination, r_ini, r_fin, c_ini, c_fin):
+    for r in range(r_ini, r_fin):
+        for c in range(c_ini, c_fin):
+            destination.cell(row=r, column=c).value = source.cell(row=r, column=c).value
+    print(destination.title + ' has been fed graciously by ' + source.title)
+      
