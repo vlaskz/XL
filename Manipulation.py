44882c526ac70a9b997e7027fb3d55914b1f9f91
Vlaskz <sam.velasquez@hotmail.com>
2021-06-06 13:11:11 -0300

    Changed file structure and division of resposibilities.

--- Manipulation.py
+++ Manipulation.py
@@ -1,29 +1,24 @@
 #Disclaimer: man stands for manipulation, and not, I'll not argue with feminists..
 import time
+import os
 
 #converts xls to xlsx using win32com library and excludes the xls files after successful conversion
-def convert_xls_xlsx(xls, erase_source_files):
+def convert_xls_xlsx(file):
     import win32com.client as win32 
-    import os  
-    file = xls
-    excel = win32.gencache.EnsureDispatch('Excel.Application')
-    wb = excel.Workbooks.Open(file)
+    xl = win32.gencache.EnsureDispatch('Excel.Application')
+    wb = xl.Workbooks.Open(file)
     wb.SaveAs(file+'x', FileFormat=51) #51 for xlsx/56 for xls
     wb.Close()
-    excel.Application.Quit()
-    if(erase_source_files):
-        os.system('cmd /c del '+file)
-        print('Source file has not been deleted.')
-    print(time.ctime(),'[convert_xls_xlsx]: ',file + ' has been converted to '+file+'x!')
+    xl.Application.Quit()
+    print(time.ctime(),'[convert_xls_xlsx]: ',file,' has been converted to ',file,'x!')
 
 #opens file in Excel for visualization
 def open_in_excel(filepath):
     from win32com.client import Dispatch
     print(time.ctime(),'[open_in_excel]: Opening in ExceL: ', filepath)
-    xl=Dispatch("Excel.Application")
-    xl.Visible=True
-    wb = xl.Workbooks.Open(filepath)
-
+    wb=Dispatch("Excel.Application")
+    wb.Visible=True
+    wb.Workbooks.Open(filepath)
 
 #fills cells with no data.
 #wb is workbook, sh is sheet, r is row, and c is column
@@ -38,11 +33,11 @@ def fetch_data(source, destination, r_ini, r_fin, c_ini, c_fin):
     for r in range(r_ini, r_fin):
         for c in range(c_ini, c_fin):
             destination.cell(row=r, column=c).value = source.cell(row=r, column=c).value
-    print(time.ctime(),'[fetch_data]: ',destination.title + ' has been graciously fed by ' + source.title)
-
+    print(time.ctime(),'[fetch_data]: ',destination.title, ' has been graciously fed by ',source.title)
+    
 #it cleans the already paid bills
-def clean_data(ws, r_ini, r_fin):
-    for r in range (r_ini, r_fin):
+def clean_data(ws, r_ini):
+    for r in range (r_ini, ws.max_row):
         s = ws.cell(row=r, column=1).value
         if(s is not None and s.find('PAGO')!=-1):#check if its None and if it is paid.
             ws.cell(row=r,column=15).value = float(0.00)
@@ -54,9 +49,15 @@ def format_currency_data(ws, c, r_ini, r_fin):
         _cell = ws.cell(column = c, row = r)
         if(_cell.value is not None and _cell.value != ''):
             _cell.value = float(str(_cell.value).replace('.','').replace(',','.'))
-            _cell.number_format ='###0.00R$'
-
+            _cell.number_format ='#,##0.00R$'
 
 def send_mail(mailing_list, file_list, message):
     print('mail sent')
+
+def move_file(origin, destination):
+    os.system('cmd /c move '+origin+' '+destination+ '/Y')
+
+def erase_file(file):
+        os.system('cmd /c del '+file)
+        print(str(file)+' file has not been deleted.')
       
--- __main__.py
+++ __main__.py
@@ -1,63 +1,68 @@
 from openpyxl import load_workbook
 import time
-import Manipulation as man, Const_Bradesco as brd #some people certainly will brag about this.
+import Manipulation as m, Const_Bradesco as b, DataSource as d, Configs as c #some people certainly will brag about this.
 
 #converts xls worksheets to xlsx - ok
-#Set erase_source_files as False to prevent it. Useful in dev stage. Copy the
+#set erase_source_files as False to prevent it. Useful in dev stage. Copy the
 #same files over and over again isn't something I appreciate.
 print(time.ctime(),'[main]: starting data workbook conversion')
-man.convert_xls_xlsx('c:\\users\\coder\\Desktop\\XL\\VE09.xls',False)
-man.convert_xls_xlsx('c:\\users\\coder\\Desktop\\XL\\AV09.xls',False)
-man.convert_xls_xlsx('c:\\users\\coder\\Desktop\\XL\\VE28.xls',False)
-man.convert_xls_xlsx('c:\\users\\coder\\Desktop\\XL\\AV28.xls',False)
+m.convert_xls_xlsx(d.VE09)
+m.convert_xls_xlsx(d.AV09)
+m.convert_xls_xlsx(d.VE28)
+m.convert_xls_xlsx(d.AV28)
 print(time.ctime(),'[main]: finishing workbook conversion')
 
 #loads the new datafiles into memory - ok
 print(time.ctime(),'[main]: starting workbook data loading')
-VE09 = load_workbook('VE09.xlsx')
-AV09 = load_workbook('AV09.xlsx')
-VE28 = load_workbook('VE28.xlsx')
-AV28 = load_workbook('AV28.xlsx')
-RECB = load_workbook('receber.xlsx', read_only=False)
+VE09X = load_workbook(d.VE09X)
+AV09X = load_workbook(d.AV09X)
+VE28X = load_workbook(d.VE28X)
+AV28X = load_workbook(d.AV28X)
+RECBX = load_workbook(d.RECBX, read_only=False)
 print(time.ctime(),'[main]: finishing workbook data loading')
 
 #clears the old data from the worksheets.
 print(time.ctime(),'[main]: starting cleaning data from main workbook')
-man.purge_data(RECB['VE28BRAD'],brd.ROW_INI,VE09.active.max_row,brd.COL_INI,brd.COL_FIN)
-man.purge_data(RECB['AV09BRAD'],brd.ROW_INI,AV09.active.max_row,brd.COL_INI,brd.COL_FIN)
-man.purge_data(RECB['VE09BRAD'],brd.ROW_INI,VE28.active.max_row,brd.COL_INI,brd.COL_FIN)
-man.purge_data(RECB['AV28BRAD'],brd.ROW_INI,AV28.active.max_row,brd.COL_INI,brd.COL_FIN)
+m.purge_data(RECBX['VE28BRAD'],b.R_INI,VE09X.active.max_row,b.C_INI,b.C_FIN)
+m.purge_data(RECBX['AV09BRAD'],b.R_INI,AV09X.active.max_row,b.C_INI,b.C_FIN)
+m.purge_data(RECBX['VE09BRAD'],b.R_INI,VE28X.active.max_row,b.C_INI,b.C_FIN)
+m.purge_data(RECBX['AV28BRAD'],b.R_INI,AV28X.active.max_row,b.C_INI,b.C_FIN)
 print(time.ctime(),'[main]: finishing cleaning data from main workbook')
 
 #copy data from datafiles to main workbook
 print(time.ctime(),'[main]: finishing copying data from data workbooks to main workbook')
-man.fetch_data(VE09.active,RECB['VE09BRAD'],brd.ROW_INI,VE09.active.max_row,brd.COL_INI,brd.COL_FIN)
-man.fetch_data(AV09.active,RECB['AV09BRAD'],brd.ROW_INI,AV09.active.max_row,brd.COL_INI,brd.COL_FIN)
-man.fetch_data(VE28.active,RECB['VE28BRAD'],brd.ROW_INI,VE28.active.max_row,brd.COL_INI,brd.COL_FIN)
-man.fetch_data(AV28.active,RECB['AV28BRAD'],brd.ROW_INI,AV28.active.max_row,brd.COL_INI,brd.COL_FIN)
+m.fetch_data(VE09X.active,RECBX['VE09BRAD'],b.R_INI,VE09X.active.max_row,b.C_INI,b.C_FIN)
+m.fetch_data(AV09X.active,RECBX['AV09BRAD'],b.R_INI,AV09X.active.max_row,b.C_INI,b.C_FIN)
+m.fetch_data(VE28X.active,RECBX['VE28BRAD'],b.R_INI,VE28X.active.max_row,b.C_INI,b.C_FIN)
+m.fetch_data(AV28X.active,RECBX['AV28BRAD'],b.R_INI,AV28X.active.max_row,b.C_INI,b.C_FIN)
 print(time.ctime(),'[main]: finishing copying data from data workbooks to main workbook')
 
 #part two: let's unbloat and unjunk the data (yes I'm a neological person)
 print(time.ctime(),'[main]: starting unbloating data')
-man.clean_data(RECB['VE09BRAD'], brd.ROW_INI, RECB['VE09BRAD'].max_row)
-man.clean_data(RECB['AV09BRAD'], brd.ROW_INI, RECB['AV09BRAD'].max_row)
-man.clean_data(RECB['VE28BRAD'], brd.ROW_INI, RECB['VE28BRAD'].max_row)
-man.clean_data(RECB['AV28BRAD'], brd.ROW_INI, RECB['AV28BRAD'].max_row)
+m.clean_data(RECBX['VE09BRAD'],b.R_INI)
+m.clean_data(RECBX['AV09BRAD'],b.R_INI)
+m.clean_data(RECBX['VE28BRAD'],b.R_INI)
+m.clean_data(RECBX['AV28BRAD'],b.R_INI)
 print(time.ctime(),'[main]: finishing unbloating data')
 
 #format data in the "O" column as float and set the field as currency (at the moment is BRL(R$))
 print(time.ctime(),'[main]: starting currency data formatting in main workbook')
-man.format_currency_data(RECB['VE09BRAD'],brd.CUR_COL,brd.ROW_INI,RECB['VE09BRAD'].max_row)
-man.format_currency_data(RECB['AV09BRAD'],brd.CUR_COL,brd.ROW_INI,RECB['AV09BRAD'].max_row)
-man.format_currency_data(RECB['VE28BRAD'],brd.CUR_COL,brd.ROW_INI,RECB['VE28BRAD'].max_row)
-man.format_currency_data(RECB['AV28BRAD'],brd.CUR_COL,brd.ROW_INI,RECB['AV28BRAD'].max_row)
+m.format_currency_data(RECBX['VE09BRAD'],b.CUR_COL,b.R_INI,RECBX['VE09BRAD'].max_row)
+m.format_currency_data(RECBX['AV09BRAD'],b.CUR_COL,b.R_INI,RECBX['AV09BRAD'].max_row)
+m.format_currency_data(RECBX['VE28BRAD'],b.CUR_COL,b.R_INI,RECBX['VE28BRAD'].max_row)
+m.format_currency_data(RECBX['AV28BRAD'],b.CUR_COL,b.R_INI,RECBX['AV28BRAD'].max_row)
 print(time.ctime(),'[main]: finishing currency data formatting in main workbook')
 
 print(time.ctime(),'[main]: saving main workbook')
-RECB.save(filename='receber.xlsx')
+RECBX.save(filename='receber.xlsx')
 
 print(time.ctime(),'[main]: opening main workbook')
-man.open_in_excel(filepath='c:\\users\\coder\\Desktop\\XL\\receber.xlsx')
+m.open_in_excel(filepath=d.RECBX)
+
+print(d.VE09,' ',c.BKP_FLD)
+print(d.AV09,' ',c.BKP_FLD)
+print(d.VE28,' ',c.BKP_FLD)
+print(d.AV28,' ',c.BKP_FLD)
 
 #Part 3: Send receber.xlsx to email.
 
